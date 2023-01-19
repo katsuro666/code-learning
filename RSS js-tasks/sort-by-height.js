ae9d0f85ec9463beaac04d1e59aa71c31aa87c6a
katsuro666 <kreepinoutdakut@gmail.com>
2023-01-19 18:30:33 +0300

    add solution for sort-by-height

---
+++ RSS js-tasks/sort-by-height.js
@@ -0,0 +1,24 @@
+/**
+ * Given an array with heights, sort them except if the value is -1.
+ *
+ * @param {Array} arr
+ * @return {Array}
+ *
+ * @example
+ * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
+ *
+ * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
+ */
+
+ function sortByHeight(arr) {
+  let newArr = arr.filter(item => item >-1)
+  newArr.sort((a,b) => a - b)
+    console.log(newArr)
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      newArr.splice(i, 0, arr[i])
+    }
+  }
+  return newArr
+}
