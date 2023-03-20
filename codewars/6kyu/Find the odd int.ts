30094514073959fb891764a0868f703a3d466134
katsuro666 <kreepinoutdakut@gmail.com>
2023-03-20 16:34:31 +0300

    add solution for 'Find the odd int '

---
+++ codewars/6kyu/Find the odd int.ts
@@ -0,0 +1,19 @@
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+const findOdd = (xs: number[]): number | void => {
+  for (let num of xs) {
+    let arrOfNumbers = xs.filter(item => item === num)
+    if (arrOfNumbers.length % 2 !== 0 || arrOfNumbers.length === 1) {
+      return num
+    }
+  }
+}
